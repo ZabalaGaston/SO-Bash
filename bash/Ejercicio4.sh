c77ffc99b63749971864a3a9a0db184f7028932a
ZabalaGaston <49344960+ZabalaGaston@users.noreply.github.com>
2019-09-16 20:03:20 -0300

    Update Ejercicio4.sh
    Ejercicio 4.sh

--- bash/Ejercicio4.sh
+++ bash/Ejercicio4.sh
@@ -1,144 +1,197 @@
+#!/bin/bash
 # Trabajo práctico N1 Ejercicio 4
 # Script: Ejercicio4.sh
 # Integrantes:
-# Zabala, Gaston        34614948
-# Pessolani, Agustin		39670584
+# Cabral, David         39757782
 # Cela, Pablo           36166857
-# Sullca, Fernando      XXXXXXXX
-# yyyyyy, David         XXXXXXXX
+# Pessolani, Agustin	39670584
+# Sullca, Fernando      37841788
+# Zabala, Gaston        34614948
 
-
-#Agregamos para que los directorios funcionen con espacios
-function func_ConfiguracionDirectorio
-{
-	IFS=$'\n'
-}
-
-# Funciones
+##################### FUNCIONES ######################
+#Función con la ayuda del script.
 function ayuda(){
 	echo ""
-  echo "Uso: ./Ejercicio2.sh directorio_recorrer extension"
-  echo "Programa que informa: "
-  echo "Cantidad de archivos analizados."
-  echo "Cantidad de líneas de código totales, como su porcentaje contra el total de líneas de los archivos analizados."
-  echo "Cantidad de líneas de comentarios"
-  echo -e "   Parametros"
-	echo -e "   \directorio_recorrer : directorio donde se buscan los archivos"
-	echo -e "   extension: extension de los archivos a analizar "
-	exit
+    echo "Uso: ./Ejercicio4.sh directorio_recorrer extension"
+    echo "Programa que informa: "
+    echo "Cantidad de archivos analizados."
+    echo "Cantidad de líneas de código totales, como su porcentaje contra el total de líneas de los archivos analizados."
+    echo "Cantidad de líneas de comentarios"
+    echo -e "Parametros"
+	echo -e "\directorio_recorrer : directorio donde se buscan los archivos"
+	echo -e "extension: extension de los archivos a analizar "
+	exit 1
 }
 
-function func_MostrarMensajeErrorParametros
-{
-	echo "Parametro Incorrecto: Utilizar Ejercicio2.sh -h para verificar funcionamiento del comando"
+function mostrarMensajeErrorParametros(){
+    echo "La cantidad de parámetros ingresados es incorrecta."
+    echo "Utilizar Ejercicio4.sh -h para verificar funcionamiento del comando"
 }
 
-function func_ValidarDirectorio
-{
+#Función que valida el directorio recibido.
+function validarDirectorio(){
 	path=$1
-
    	if ! test -e $path
 	then
 		echo "Parametro Incorrecto: Ruta ingresada no existente"
-		exit
+		exit 2
 	fi
 
 	if ! test -d $path
 	then
 		echo "Parametro Incorrecto: Ruta ingresada no es directorio"
-		exit
+		exit 3
 	fi
 }
 
-function func_validarParametros
-{
+#Función que valida el primer parámetro recibido.
+function validarPrimerParametro(){
 	if [ "$1" == "-h" ] || [ "$1" == "-?" ] || [ "$1" == "--help" ]
 	then
 		ayuda
-		exit
+        exit 4
 	fi
-
-    if test $# -gt 2 ; then
-        func_MostrarMensajeErrorParametros
-        exit
-    fi
-
-	func_ValidarDirectorio $1
+	validarDirectorio $1
 }
 
+#Función para obtener la cantidad de líneas.
 function obtenerCantLineas(){
-  _aux=`wc -l $j`
-  _cantLineasV=(${_aux// / })
-  cantLineas=`expr ${_cantLineasV[0]} + $cantLineas`
-}
-
-function obtenerCantLineasComentadas(){
-  _AuxcantLineasComentadas=$(awk '
-    BEGIN{cant=0;
-       flag=0
+    _AuxcantLineasTotales=$(awk '
+    BEGIN{
+        cantLineas=0;
     }
     {
-      if($0 ~ "^//")
-        cant+=100;
-
-      if($0 ~ "/$" && flag == 1){
-        cant+=1;
-        flag=0;
-      }
-
-      #Esta condicion no está funcionando
-      if($0 ~ "^/*"){
-        cant+=1;
-        flag=1;
-      }
+        cantLineas++;
     }
     END{
-     print cant;
+        print cantLineas;
     }' $j)
+    cantLineasTotales=`expr $_AuxcantLineasTotales + $cantLineasTotales`
 }
 
+#Función para obtener la cantidad de líneas de código.
 function obtenerCantLineasCodigo(){
-  _AuxcantLineasCodigo=$(awk 'BEGIN{cant=0}{
-       cant=2;
+    _AuxcantLineasCodigo=$(awk '
+    BEGIN{
+        cantLineasCodigo = 0;
+        flagComentarioMulti = 0;
+        flagYaConte = 0;
+    }
+    $0 != NULL && !($0 ~ /^\/\//) && !($0 ~ /^\/\*/) && flagComentarioMulti == 0{
+        cantLineasCodigo++;
+        flagYaConte = 1;
+    }
+    $0 ~ /\/\*/{
+        flagComentarioMulti = 1;
+    }
+    $0 ~ /\*\//{
+        flagComentarioMulti = 0;
+    }
+    flagYaConte == 0 && flagComentarioMulti == 0 && !($0 ~ /\*\/$/){
+        cantLineasCodigo++;
+    }
+    {
+        flagYaConte = 0;
     }
     END{
-     print cant;
+        print cantLineasCodigo;
     }' $j)
+    cantLineasCodigo=`expr $cantLineasCodigo + $_AuxcantLineasCodigo`
 }
 
-function acumularCantLineasComentadas(){
-  acumCantLineasComentadas=`expr $acumCantLineasComentadas + $_AuxcantLineasComentadas`
-  }
+#Función para obtener la cantidad de líneas comentadas.
+function obtenerCantLineasComentadas(){
+    _AuxcantLineasComentadas=$(awk '
+    BEGIN{
+        cantLineasComentadas = 0;
+        flagComentarioMulti = 0;
+        flagYaConte = 0;
+    }
+    $0 ~ /\/\// || flagComentarioMulti == 1{
+        cantLineasComentadas++;
+        flagYaConte = 1;  
+    }
+    $0 ~ /\/\*/ { 
+        if(flagYaConte == 0)
+            cantLineasComentadas++;
+        flagComentarioMulti = 1;
+    }
+    $0 ~ /\*\//{ 
+        flagComentarioMulti = 0;
+    }
+    {
+        flagYaConte = 0;
+    }   
+    END{
+        print cantLineasComentadas;
+    }' $j)
+    cantLineasComentadas=`expr $cantLineasComentadas + $_AuxcantLineasComentadas`
+}
 
-function acumularCantLineasCodigo(){
-  acumCantLineasCodigo=`expr $acumCantLineasCodigo + $_AuxcantLineasCodigo`
-  }
+#Función para obtener el porcentaje de líneas de código.
+function obtenerPorcentajeLineasCodigo(){
+    porcentajeLineasCodigo=$(awk -v cantLineasCodigo="$cantLineasCodigo" -v cantLineasTotales="$cantLineasTotales" '
+    BEGIN{ 
+        print ((cantLineasCodigo / cantLineasTotales) * 100)
+    }')
+}
 
-#func_ConfiguracionDirectorio()
+#Función para obtener el porcentaje de líneas comentadas.
+function obtenerPorcentajeLineasComentadas(){
+    porcentajeLineasComentadas=$(awk -v cantLineasComentadas="$cantLineasComentadas" -v cantLineasTotales="$cantLineasTotales" '
+    BEGIN{ 
+        print ((cantLineasComentadas / cantLineasTotales) * 100)
+    }')
+}
 
-func_validarParametros $1
+##################### INICIO DEL SCRIPT ##############
+#Asignación para que el script pueda trabajar con rutas con espacios.
+IFS='
+'
+#Validaciones de los parámetros.
+validarPrimerParametro $1
+if test $# -ne 2 
+then
+    mostrarMensajeErrorParametros
+    exit 5
+fi
 
-aBuscar=$2
+#Inicialización de variables
 path=$1
-cantLineas=0
+aBuscar=$2 #Extensión de archivos a buscar
+
 cantArchivos=0
-acumCantLineasComentadas=0
-acumCantLineasCodigo=0
+cantLineasTotales=0
+cantLineasCodigo=0
+cantLineasComentadas=0
 
-for j in `find $path -name "*$aBuscar"`
+for j in `find $path -type f -name "*$aBuscar"`
 do
-  _AuxcantLineasComentadas=0
-  _AuxcantLineasCodigo=0
-  cantArchivos=`expr $cantArchivos + 1`
-  obtenerCantLineas $j
-  obtenerCantLineasComentadas
-  acumularCantLineasComentadas
+    #Auxiliares que guardaran las líneas correspondientes al archivo actual.
+    _AuxcantLineasComentadas=0
+    _AuxcantLineasCodigo=0
 
-  obtenerCantLineasCodigo
-  acumularCantLineasCodigo
+    
+    obtenerCantLineas $j
+    obtenerCantLineasComentadas
+    obtenerCantLineasCodigo
+    
+    cantArchivos=`expr $cantArchivos + 1` 
 done
+#Muestro los resultados
+echo "Cantidad de archivos analizados:          "$cantArchivos"."
+#Condición para el caso en que los archivos estén vacíos.
+if [ $cantLineasTotales -gt 0 ]
+then
+    porcentajeLineasCodigo=0
+    porcentajeLineasComentadas=0
+    obtenerPorcentajeLineasCodigo
+    obtenerPorcentajeLineasComentadas
+    echo "Cantidad de líneas de Codigo totales:     "$porcentajeLineasCodigo"%."
+    echo "Cantidad de líneas Comentadas totales:    "$porcentajeLineasComentadas"%."
+else
+    echo "La cantidad de líneas es de cero."
+fi
 
-echo "Cantidad de lineas: "$cantLineas
-echo "Cantidad de archivos: "$cantArchivos
-echo "Cantidad líneas comentadas: "$acumCantLineasComentadas
-echo "Cantidad líneas Codigo: "$acumCantLineasCodigo
+exit 0
+##################### FIN DEL SCRIPT #################
